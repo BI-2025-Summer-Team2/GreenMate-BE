4b9e404fe9cfecd54fd2203b509827c2463300b0
konempty <jameswebb@naver.com>
2025-07-03 00:47:39 +0900

    TEAM2-53 제미나이 리뷰 반영

--- src/main/java/kr/bi/greenmate/config/properties/ObjectStorageProperties.java
+++ src/main/java/kr/bi/greenmate/config/properties/ObjectStorageProperties.java
@@ -1,11 +1,13 @@
 package kr.bi.greenmate.config.properties;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "spring.cloud.aws.s3")
 public class ObjectStorageProperties {
-    public final String bucket;
-    public final String cdnUrl;
+    private final String bucket;
+    private final String cdnUrl;
 }
