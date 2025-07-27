8603cdf32ca075d59c4ed9c317db8c7d9955bd8b
jyu <jy041@korea.ac.kr>
2025-07-27 22:23:40 +0900

    User Entity 추가
    GreenMateApplication : @EnableJpaAuditing 추가
    자동 정보 기록
    BaseTimeEntity : 시간 관련 공통 필드 상속을 위한 추상 클래스

---
+++ src/main/java/kr/bi/greenmate/user/domain/User.java
@@ -0,0 +1,40 @@
+package kr.bi.greenmate.user.domain;
+
+import jakarta.persistence.*;
+import kr.bi.greenmate.common.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR2(10 CHAR)", unique = true)
+    private String nickname;
+
+    @Column(length = 63)
+    private String profileImageUrl;
+
+    @Column(columnDefinition = "VARCHAR2(300 CHAR)")
+    private String selfIntroduction;
+
+    private LocalDateTime deletedAt;
+}
