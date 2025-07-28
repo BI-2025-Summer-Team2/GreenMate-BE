229552a3e0f127221492da1dfcf9379957873a92
Jihyun-Choi <034179@naver.com>
2025-07-28 20:23:02 +0900

    TEAM2-60 환경 활동 모집 부가 기능 엔티티 구현
    - 환경 활동 공고 팀원(GreenTeamParticipant) 엔티티
    - 환경 활동 공고 좋아요(GreenTeamPostLike) 엔티티
    - 환경 활동 공고 댓글(GreenTeamComment) 엔티티

---
+++ src/main/java/kr/bi/greenmate/green_team_post/domain/GreenTeamPostComment.java
@@ -0,0 +1,44 @@
+package kr.bi.greenmate.green_team_post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import kr.bi.greenmate.common.domain.SoftDeletableEntity;
+import kr.bi.greenmate.user.domain.User;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GreenTeamPostComment extends SoftDeletableEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private GreenTeamPost post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false, length = 300)
+  private String content;
+
+  @Column(length = 63)
+  private String imageUrl;
+}
