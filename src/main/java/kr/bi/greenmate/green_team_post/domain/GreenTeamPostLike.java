c1b82b91613950e76586ae36bf45eac09fb29eb3
Jihyun-Choi <034179@naver.com>
2025-07-28 20:23:02 +0900

    TEAM2-60 환경 활동 모집 부가 기능 엔티티 구현
    - 환경 활동 공고 팀원(GreenTeamParticipant) 엔티티
    - 환경 활동 공고 좋아요(GreenTeamPostLike) 엔티티
    - 환경 활동 공고 댓글(GreenTeamComment) 엔티티

---
+++ src/main/java/kr/bi/greenmate/green_team_post/domain/GreenTeamPostLike.java
@@ -0,0 +1,45 @@
+package kr.bi.greenmate.green_team_post.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import kr.bi.greenmate.common.domain.CreatedOnlyEntity;
+import kr.bi.greenmate.user.domain.User;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uq_green_team_post_like_post_user", columnNames = {"post_id",
+            "user_id"})
+    }
+)
+public class GreenTeamPostLike extends CreatedOnlyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private GreenTeamPost post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+}
