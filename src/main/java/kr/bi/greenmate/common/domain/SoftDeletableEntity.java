9392029f8aa2921d004c0bfabb61c425e4ff5194
Jihyun-Choi <034179@naver.com>
2025-07-30 02:17:20 +0900

    TEAM2-60 @SQLDelete 기반 소프트 삭제 방식 적용
    - delete(entity) 호출 시 실제 삭제 대신 deleted_at 값을 업데이트하도록 변경
    - SoftDeletableEntity 추상 클래스의 softDelete() 메서드 제거
      - @SQLDelete가 동일한 기능을 수행하므로 중복 로직 제거

--- src/main/java/kr/bi/greenmate/common/domain/SoftDeletableEntity.java
+++ src/main/java/kr/bi/greenmate/common/domain/SoftDeletableEntity.java
@@ -5,16 +5,22 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 
+/**
+ * 소프트 삭제용 추상 클래스입니다.
+ * <p>
+ * - deleted_at 컬럼을 기준으로 삭제 여부를 판단합니다.
+ * @Where(clause = "deleted_at IS NULL")을 통해 기본 조회에서 삭제된 데이터는 제외됩니다.
+ * <p>
+ * 사용 시 각 엔티티에 아래 어노테이션을 반드시 선언해야 합니다:
+ *
+ * @org.hibernate.annotations.SQLDelete(sql = "UPDATE 테이블명 SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+ */
 @Getter
 @MappedSuperclass
 public abstract class SoftDeletableEntity extends BaseTimeEntity {
 
   @Column
   protected LocalDateTime deletedAt;
-
-  public void softDelete() {
-    this.deletedAt = LocalDateTime.now();
-  }
 
   public boolean isDeleted() {
     return deletedAt != null;
