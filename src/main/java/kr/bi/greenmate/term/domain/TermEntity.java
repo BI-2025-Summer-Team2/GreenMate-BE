28e325f87088fae05684de7c24373e1064e66a35
jyu <jy041@korea.ac.kr>
2025-08-02 19:50:45 +0900

    TEAM2-57 TermEntity 추가

---
+++ src/main/java/kr/bi/greenmate/term/domain/TermEntity.java
@@ -0,0 +1,34 @@
+package kr.bi.greenmate.term.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class TermEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "CLOB", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer mandatory;
+
+}
