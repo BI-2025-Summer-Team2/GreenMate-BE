dff96526f73d5af3079bc5c28bce992f9efadced
jyu <jy041@korea.ac.kr>
2025-08-02 14:48:58 +0900

    TEAM2-56 2차 캐싱, 와일드카드 import 제거
    intellij 설정 변경했습니다. 앞으로도 와일드카드 import는 안 하도록 하겠습니다.

--- src/main/java/kr/bi/greenmate/user/domain/User.java
+++ src/main/java/kr/bi/greenmate/user/domain/User.java
@@ -1,21 +1,23 @@
 package kr.bi.greenmate.user.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import kr.bi.greenmate.common.domain.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.NaturalId;
 import org.hibernate.annotations.NaturalIdCache;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="users")
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Table(name = "users")
 @NaturalIdCache
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @AllArgsConstructor
