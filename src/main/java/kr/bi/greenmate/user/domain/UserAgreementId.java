91a014a4d3ae01a362783148df9a60ae6a2f4917
jyu <jy041@korea.ac.kr>
2025-08-03 14:46:51 +0900

    TEAM2-57 UserAgreement의 복합키 클래스 생성

---
+++ src/main/java/kr/bi/greenmate/user/domain/UserAgreementId.java
@@ -0,0 +1,17 @@
+package kr.bi.greenmate.user.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+public class UserAgreementId {
+    private Long userId;
+    private Long termId;
+}
