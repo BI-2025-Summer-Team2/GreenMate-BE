3c8492b534b62c0b6d56adaf74e1381032f99ca5
Jihyun-Choi <034179@naver.com>
2025-08-17 22:45:16 +0900

    TEAM2-13 FQCN 대신 LocationType import 사용으로 가독성 개선

--- src/main/java/kr/bi/greenmate/green_team_post/dto/GreenTeamPostCreateRequest.java
+++ src/main/java/kr/bi/greenmate/green_team_post/dto/GreenTeamPostCreateRequest.java
@@ -10,6 +10,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import kr.bi.greenmate.green_team_post.domain.LocationType;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -32,7 +35,7 @@ public class GreenTeamPostCreateRequest {
   private String content;
 
   @NotNull
-  private kr.bi.greenmate.green_team_post.domain.LocationType locationType;
+  private LocationType locationType;
 
   @NotBlank
   private String locationGeojson; // 프론트에서 전달받은 GeoJSON 그대로 저장
