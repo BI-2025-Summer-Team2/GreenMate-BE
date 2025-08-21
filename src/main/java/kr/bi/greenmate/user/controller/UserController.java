dc0ea4e92a059dfcdc9785d2bf6806a31c0b49e6
jyu <jy041@korea.ac.kr>
2025-08-21 11:09:14 +0900

    TEAM2-11 swagger에서 사용할 request의 userInfo를 json으로 지정

--- src/main/java/kr/bi/greenmate/user/controller/UserController.java
+++ src/main/java/kr/bi/greenmate/user/controller/UserController.java
@@ -2,8 +2,11 @@ package kr.bi.greenmate.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Encoding;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import kr.bi.greenmate.common.annotation.SwaggerBody;
 import kr.bi.greenmate.user.dto.SignUpRequest;
 import kr.bi.greenmate.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +29,7 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "회원가입", description = "사용자의 정보(JSON)와 프로필 이미지(파일)를 받아 회원으로 등록합니다.")
+    @SwaggerBody(content = @Content(encoding = @Encoding(name = "userInfo", contentType = MediaType.APPLICATION_JSON_VALUE)))
     @PostMapping(value = "/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> signUp(@RequestPart("userInfo") @Valid SignUpRequest request,
                                        @RequestPart(value = "profileImage", required = false) @Parameter(description = "프로필 이미지 파일") MultipartFile profileImage) {
