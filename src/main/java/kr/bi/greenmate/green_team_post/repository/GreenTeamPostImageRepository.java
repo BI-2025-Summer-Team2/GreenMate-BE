215e1139c2aa8bde27dfd5de4d52371bae5c0312
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2025-08-23 23:52:45 +0900

    Revert "TEAM2-13 환경 활동 모집 글 작성 API 구현"

--- src/main/java/kr/bi/greenmate/green_team_post/repository/GreenTeamPostImageRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.bi.greenmate.green_team_post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.bi.greenmate.green_team_post.domain.GreenTeamPostImage;
-
-public interface GreenTeamPostImageRepository extends JpaRepository<GreenTeamPostImage, Long> {
-
-}
--- src/main/java/kr/bi/greenmate/green_team_post/repository/GreenTeamPostRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.bi.greenmate.green_team_post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.bi.greenmate.green_team_post.domain.GreenTeamPost;
-
-public interface GreenTeamPostRepository extends JpaRepository<GreenTeamPost, Long> {
-
-}
